a2c75635f653c72215c6b8660182666299952c6a
Patric Schönenberger <p.schoenenberger@spedition.de>
2023-02-01 23:58:05 +0100

    Link Classes in Main Program

---
+++ src/Paypal.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Paypal;
+use Paypal\CatalogProducts;
+use Paypal\Disputes;
+use Paypal\Identify;
+use Paypal\Invoice;
+use Paypal\Order;
+use Paypal\PartnerReferral;
+use Paypal\PaymentExperience;
+use Paypal\Payment;
+use Paypal\ReferencedPayout;
+use Paypal\Subscription;
+use Paypal\Tracking;
+use Paypal\TransactionSearch;
+
+
+class Paypal
+{
+
+}
